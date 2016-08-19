2c16745274d2850f69efb7c705c2a92d9effb59e
Elemshalit <elemshalit@gmail.com>
2016-08-18 18:28:51 -0700

    add subjects model - initial

---
+++ app/controllers/subjects_controller.rb
@@ -0,0 +1,32 @@
+class SubjectsController < ApplicationController
+
+  before_action :find_issue
+  before_action :find_subject, only: [:show]
+
+  def new
+    @subject = @issue.subjects.new
+end
+
+def create
+  @subject = @issue.subjects.new subject_params
+  if @subject.save
+    redirect_to issue_subject_path(@issue)
+  else
+    render 'new'
+ end
+end
+
+private
+
+def subject_params
+  params.require(:subject).permit(:title, :description)
+end
+
+def find_subject
+  @subject = Issue.find(params[:subject_id])
+end
+
+def find_subject
+  @subject = Subject.find(params[:id])
+ end
+end
