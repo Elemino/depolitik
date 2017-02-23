3eaab6d5cb271c23d056333d36cd5a94621516ca
Elemino <elemshalit@gmail.com>
2017-02-22 22:53:26 -0800

    Add static about and update some content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  get 'static_pages/about'
+
   get "/pages/:page" => "pages#show"
+
+  get "/about.html", to: "static_pages#about", as: "about"
 
   devise_for :issues
 
