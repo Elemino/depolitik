5af94d99a80b49d33c061bc4016b4ddbf2e54b96
Elemino <elemshalit@gmail.com>
2018-01-13 23:31:17 -0800

    Update application.rb

--- app/assets/javascripts/application.js.rb
+++ app/assets/javascripts/application.js.rb
@@ -4,4 +4,22 @@ require 'turbolinks'
 require_tree '.'
 
 
-puts "hello world!"
+puts "Hello world!"
+pp hello: :world
+require 'console'
+$console.log %w[Hello world!]
+
+
+require 'native'
+$$.alert "Hello world!"
+
+
+require 'opal-jquery'
+Document.ready? do
+  Element.find('body').html = '<h1>Hello world!</h1>'
+end
+
+
+$$[:document].addEventListener(:DOMContentLoaded, -> {
+  $$[:document].querySelector('body')[:innerHTML] = '<h1>Hello world!</h1>'
+})
