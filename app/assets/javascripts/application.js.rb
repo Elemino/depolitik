c59c2f7410446346b46f2abf362e1d8e27a63a0f
Elemino <elemshalit@gmail.com>
2018-04-21 21:28:37 -0700

    Remove everything in application.rb and return tree

--- app/assets/javascripts/application.js.rb
+++ app/assets/javascripts/application.js.rb
@@ -1,17 +1,2 @@
-require 'turbolinks'
-
-puts "Hello world!"
-hello::world
-require 'console'
-$console.log %w[Hello world!]
-
-
-require 'native'
-$$.alert "Hello world!"
-
-
-
-
-$$[:document].addEventListener(:DOMContentLoaded, -> {
-  $$[:document].querySelector('body')[:innerHTML] = '<h1>Hello world!</h1>'
-})
+require_tree '.'
+ 
